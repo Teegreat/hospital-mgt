2bf3065d1096191bc07891f1535613671c6bd323
TeeGreat <toyinatofarati64@gmail.com>
2024-08-24 17:14:34 +0100

    passkey implemented

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,12 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home({searchParams}: SearchParamProps) {
-
-  const isAdmin = searchParams.admin === 'true'
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
 
   return (
     <div className="flex h-screen max-h-screen">
-
-    {isAdmin && <PasskeyModal />}
+      {isAdmin && <PasskeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
@@ -26,7 +24,9 @@ export default function Home({searchParams}: SearchParamProps) {
           <PatientForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarePulse</p>
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
             </Link>
@@ -34,7 +34,7 @@ export default function Home({searchParams}: SearchParamProps) {
         </div>
       </section>
 
-      <Image 
+      <Image
         src="/assets/images/onboarding-img.png"
         height={1000}
         width={1000}
